532a0b8f281c96680c50d103df7ce0e0796b412e
karthi <karthikeyans@vkss.tech>
2025-10-08 15:32:29 +0530

    Initial commit

---
+++ src/category.js
@@ -0,0 +1,27 @@
+import whiskey from "./assets/whiskey-img.jpg";
+import beer from "./assets/beer-img.jpg";
+import vodka from "./assets/vodka-img.jpg";
+import rum from "./assets/rum-img.jpg";
+import gin from "./assets/gin-img.jpg";
+import brandy from "./assets/brandy-img.jpg";
+import wine from "./assets/wine-img.jpg";
+import cocktails from "./assets/cocktails-img.jpg";
+import liqueurs from "./assets/liqueurs-img.jpg";
+import tequila from "./assets/tequila-img.jpg";
+import mead from "./assets/mead-img.jpg";
+import allLiqours from "./assets/all-liqours.jpg";
+
+export const categories = [
+  { category: "Whiskey", image: whiskey },
+  { category: "Beer", image: beer },
+  { category: "Vodka", image: vodka },
+  { category: "Rum", image: rum },
+  { category: "Gin", image: gin },
+  { category: "Brandy", image: brandy },
+  { category: "Wine", image: wine },
+  { category: "Cocktails", image: cocktails },
+  { category: "Liqueurs", image: liqueurs },
+  { category: "Tequila", image: tequila },
+  { category: "Mead", image: mead },
+  { category: "All", image: allLiqours },
+];
